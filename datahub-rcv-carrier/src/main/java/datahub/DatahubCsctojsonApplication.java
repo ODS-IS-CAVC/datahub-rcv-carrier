d094bab1e5fc87f76aca5432a107272d8651c2cb
takahashihiroto-f <takahashihiroto@fujitsu.com>
2025-03-24 18:57:33 +0900

    Create DatahubCsctojsonApplication.java

---
+++ datahub-rcv-carrier/src/main/java/datahub/DatahubCsctojsonApplication.java
@@ -0,0 +1,16 @@
+package datahub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * メイン処理
+ */
+@SpringBootApplication
+public class DatahubCsctojsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatahubCsctojsonApplication.class, args);
+	}
+
+}
